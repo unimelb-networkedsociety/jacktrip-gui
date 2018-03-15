59d1256c960e55aea9f813ccd85d00512c3347f3
yunhan0 <yunhn.lee@gmail.com>
2018-03-15 16:27:53 +1100

    Initial Commit

---
+++ JacktripGui/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JacktripGui
+//
+//  Created by Yunhan on 15/3/18.
+//  Copyright © 2018 NSI Lab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JacktripGui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JacktripGui
+//
+//  Created by Yunhan on 15/3/18.
+//  Copyright © 2018 NSI Lab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
