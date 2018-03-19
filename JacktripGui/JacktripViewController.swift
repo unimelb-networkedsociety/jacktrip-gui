7a68e0870f730da7cc6ee6fe6a45f4bad1ebb3a3
yunhan0 <yunhn.lee@gmail.com>
2018-03-19 14:21:24 +1100

    basic shell script invoke function

--- JacktripGui/JacktripViewController.swift
+++ JacktripGui/JacktripViewController.swift
@@ -12,12 +12,12 @@ import Foundation
 class JacktripViewController: NSViewController {
     var jacktripTask: Process!
 
+    @IBOutlet var log: NSTextView!
     @IBOutlet weak var ip: NSTextField!
     @IBOutlet weak var port: NSTextField!
     @IBAction func connectToServer(_ sender: NSButtonCell) {
         // shell command to connect: jacktrip -c [ip] [port]
-        shell(args:["say","hello"])
-        print("connect to", ip.stringValue, port.stringValue)
+        shell(args:["-c",ip.stringValue, port.stringValue])
     }
     
     override func viewDidLoad() {
@@ -35,23 +35,25 @@ class JacktripViewController: NSViewController {
     func shell(args: [String]) {
 
         self.jacktripTask = Process()
-        self.jacktripTask.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+        self.jacktripTask.executableURL = URL(fileURLWithPath: "/usr/local/bin/jacktrip")
         self.jacktripTask.arguments = args
-        
 
-        try! self.jacktripTask.run()
-        if !self.jacktripTask.isRunning {
-            let status = self.jacktripTask.terminationStatus
-            if status == 0 {
-                print("Task succeeded.")
+        let pipe = Pipe()
+        self.jacktripTask.standardOutput = pipe
+        self.jacktripTask.standardError = pipe
+        let outHandle = pipe.fileHandleForReading
+        
+        outHandle.readabilityHandler = { pipe in
+            if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
+                DispatchQueue.main.async { // Correct
+                    self.log.string += "\(line)\n"
+                }
             } else {
-                print("Task failed.")
+                print("Error decoding data: \(pipe.availableData)")
             }
         }
         
-
-        //self.jacktripTask.waitUntilExit()
-        //return output
-    
+        try! self.jacktripTask.run()
     }
 }
+
