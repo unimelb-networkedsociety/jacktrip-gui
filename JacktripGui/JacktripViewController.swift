142a9985cbb5b1b7ebb2e90ca86ac1b9fbc3139b
yunhan0 <yunhn.lee@gmail.com>
2018-03-19 15:50:26 +1100

    basic table view

--- JacktripGui/JacktripViewController.swift
+++ JacktripGui/JacktripViewController.swift
@@ -11,19 +11,34 @@ import Foundation
 
 class JacktripViewController: NSViewController {
     var jacktripTask: Process!
+    let portData = [
+        ["port": "1", "operation": "connect"],
+        ["port": "2", "operation": "connect"],
+        ["port": "3", "operation": "connect"],
+        ["port": "4", "operation": "connect"]
+    ]
+    
 
-    @IBOutlet var log: NSTextView!
+    @IBOutlet var serverTableView: NSTableView!
+    
+    @IBOutlet var logTextView: NSTextView!
+    
     @IBOutlet weak var ip: NSTextField!
     @IBOutlet weak var port: NSTextField!
     @IBAction func connectToServer(_ sender: NSButtonCell) {
         // shell command to connect: jacktrip -c [ip] [port]
+        print(ip.stringValue, port.stringValue)
         shell(args:["-c",ip.stringValue, port.stringValue])
     }
-    
+
+    @IBAction func startServer(_ sender: NSButton) {
+        print("starting server")
+        shell(args: ["-s"])
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.serverTableView.delegate = self
+        self.serverTableView.dataSource = self
     }
 
     override var representedObject: Any? {
@@ -46,7 +61,7 @@ class JacktripViewController: NSViewController {
         outHandle.readabilityHandler = { pipe in
             if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
                 DispatchQueue.main.async { // Correct
-                    self.log.string += "\(line)\n"
+                    self.logTextView.string += "\(line)\n"
                 }
             } else {
                 print("Error decoding data: \(pipe.availableData)")
@@ -55,5 +70,26 @@ class JacktripViewController: NSViewController {
         
         try! self.jacktripTask.run()
     }
+}
+
+extension JacktripViewController:NSTableViewDataSource, NSTableViewDelegate{
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return portData.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?{
+        
+        if tableColumn?.identifier.rawValue == "operation"{
+            let result = tableView.makeView(withIdentifier:NSUserInterfaceItemIdentifier(rawValue: "operation"), owner: self) as! NSButton
+            result.title = portData[row]["operation"]!
+            return result
+        } else {
+            let result = tableView.makeView(withIdentifier:(tableColumn?.identifier)!, owner: self) as! NSTableCellView
+            result.textField?.stringValue = portData[row][(tableColumn?.identifier.rawValue)!]!
+            return result
+        }
+        
+    }
+    
 }
 
